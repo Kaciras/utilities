ba0f2f20acf708e0e2d814dde8e99501e5a5f61a
Kaciras <Kaciras@protonmail.com>
2022-03-13 15:23:17 +0800

    fix format.ts

--- lib/format.ts
+++ lib/format.ts
@@ -3,6 +3,8 @@ const SIZE_UNITS = ["", "K", "M", "G", "T", "P", "E", "Z", "Y"];
 /**
  * Convert bytes to a human-readable string.
  *
+ * The result may lose precision and cannot be converted back.
+ *
  * @param value The number to format.
  * @param fraction 1000 for SI or 1024 for IEC.
  */
@@ -10,20 +12,27 @@ export function formatSize(value: number, fraction: 1024 | 1000 = 1024) {
 	const size = Math.abs(value);
 
 	if (size === 0) {
-		return `${value.toFixed(2)} B`;
+		return `${value} B`;
 	}
+	let exponent = ~~(Math.log2(size) / Math.log2(fraction));
+	exponent = Math.min(exponent, SIZE_UNITS.length - 1);
 
-	const i = ~~(Math.log2(size) / Math.log2(fraction));
-	const v = value / (fraction ** i);
-	return `${v.toFixed(2)} ${SIZE_UNITS[i]}B`;
+	const v = value / (fraction ** exponent);
+
+	return `${Number(v.toFixed(2))} ${SIZE_UNITS[exponent]}B`;
 }
 
 /**
+ * Parse size string to number of bytes.
  *
- *
- * @param value
+ * @param value The size string.
  * @param fraction 1000 for SI or 1024 for IEC.
  */
 export function parseSize(value: string, fraction: 1024 | 1000 = 1024) {
-
+	const match = /^([-+0-9.]+)\s*(\w)?B$/.exec(value);
+	if (!match) {
+		throw new Error(`Can not parse size: "${value}"`);
+	}
+	const [, v, u = ""] = match;
+	return Number(v) * (fraction ** SIZE_UNITS.indexOf(u));
 }
