decd6f30bddf8a01175a4904c7d74ca7e09b3cd6
Kaciras <Kaciras@protonmail.com>
2022-03-13 10:26:27 +0800

    improve types for event.ts

--- lib/event.ts
+++ lib/event.ts
@@ -1,9 +1,9 @@
-type Handler<T> = (event: T) => void;
+type Handler<T extends any[]> = (...args: T) => void;
 
 /**
  * 一个最简单的单事件 EventEmitter 实现，适用于少量并确定的事件。
  */
-export class SingleEventEmitter<T> {
+export class SingleEventEmitter<T extends any[] = any[]> {
 
 	private readonly handlers: Array<Handler<T>> = [];
 
@@ -23,29 +23,37 @@ export class SingleEventEmitter<T> {
 		this.handlers.length = 0;
 	}
 
-	dispatchEvent(event: T) {
-		for (const fn of this.handlers) fn(event);
+	dispatchEvent(...args: T) {
+		for (const fn of this.handlers) fn(...args);
 	}
 }
+
+type EventsMap = Record<string, any>;
 
 /**
  * 处理器存储，键是事件名值是处理器列表，因为太长所以拿出来了。
  */
-type HandlerMap = Record<string, Array<Handler<any>>>;
+type HandlerMap<T extends EventsMap> = Partial<{
+	[K in keyof T]: Array<T[K]>;
+}>;
+
+interface Default extends EventsMap {
+	[event: string]: (...args: any) => void;
+}
 
 /**
  * 一个最简单的多事件 EventEmitter 实现，适用于类型不确定的事件。
  */
-export class MultiEventEmitter {
+export class MultiEventEmitter<T extends EventsMap = Default> {
 
-	private events: HandlerMap = Object.create(null);
+	private events: HandlerMap<T> = Object.create(null);
 
-	addListener(name: string, handler: Handler<any>) {
+	addListener<K extends keyof T>(name: K, handler: T[K]) {
 		const { events } = this;
-		(events[name] ??= []).push(handler);
+		(events[name] ??= [] as Array<T[K]>).push(handler);
 	}
 
-	removeListener(name: string, handler: Handler<any>) {
+	removeListener<K extends keyof T>(name: K, handler: T[K]) {
 		const { events } = this;
 		const handlers = events[name];
 
@@ -63,7 +71,7 @@ export class MultiEventEmitter {
 		}
 	}
 
-	removeAllListeners(name?: string) {
+	removeAllListeners(name?: keyof T) {
 		if (typeof name === "string") {
 			delete this.events[name];
 		} else {
@@ -71,7 +79,10 @@ export class MultiEventEmitter {
 		}
 	}
 
-	dispatchEvent(name: string, event: any) {
-		for (const fn of this.events[name]) fn(event);
+	dispatchEvent<K extends keyof T>(name: K, ...args: Parameters<T[K]>) {
+		const handlers = this.events[name];
+		for (const fn of handlers ?? []) fn(...args);
 	}
 }
+
+// TODO: once
