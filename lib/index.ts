7b4a76025dc6f42bdd6c5833a869cd627cca61f8
Kaciras <Kaciras@protonmail.com>
2022-03-14 11:07:59 +0800

    add facade module `index.ts`

---
+++ lib/index.ts
@@ -0,0 +1,7 @@
+export * from "./codec.js";
+export * from "./crypto.js";
+export * from "./event.js";
+export * from "./format.js";
+export * from "./fetch.js";
+export * from "./misc.js";
+export * from "./interactive.js";
