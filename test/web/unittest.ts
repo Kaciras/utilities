b140a79febf4bddd4e65253211d38a8a77dc7d0b
Kaciras <Kaciras@protonmail.com>
2023-06-29 09:07:17 +0800

    fix playwright route

--- test/web/unittest.ts
+++ test/web/unittest.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from "fs";
 import swc from "@swc/core";
-import { expect, test as base } from "@playwright/test";
+import { expect, Request, Route, test as base } from "@playwright/test";
 
 export { expect };
 
@@ -14,17 +14,19 @@ const BlankHTML = {
 	body: "<html><head></head><body></body></html>",
 };
 
+function loadIndexAndModule(route: Route, request: Request) {
+	const url = request.url();
+	if (url === baseURL) {
+		return route.fulfill(BlankHTML);
+	}
+	const path = decodeURIComponent(url.slice(baseURL.length));
+	const { code: body } = swc.transformFileSync(path, swcrc);
+	return route.fulfill({ body, contentType: "text/javascript" });
+}
+
 export const test = base.extend({
 	page: async ({ page }, use) => {
-		await page.route("**/*", (route, request) => {
-			if (request.url() === baseURL) {
-				return route.fulfill(BlankHTML);
-			}
-			const path = decodeURIComponent(request.url().slice(baseURL.length));
-			const { code: body } = swc.transformFileSync(path, swcrc);
-			return route.fulfill({ body, contentType: "text/javascript" });
-		});
-
+		await page.route(`${baseURL}**/*`, loadIndexAndModule);
 		await page.goto(baseURL);
 
 		// Coverage APIs are only supported on Chromium-based browsers.
