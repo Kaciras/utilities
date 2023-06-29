7e7295bc43ba993acf32c31d5e19ac1069823623
Kaciras <Kaciras@protonmail.com>
2023-06-29 11:55:51 +0800

    add coverage support for browser test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,4 @@
+import { env } from "process";
 import { defineConfig, devices } from "@playwright/test";
 
 /**
@@ -8,11 +9,11 @@ export default defineConfig({
 	/* Run tests in files in parallel */
 	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
-	forbidOnly: !!process.env.CI,
+	forbidOnly: !!env.CI,
 	/* Retry on CI only */
-	retries: process.env.CI ? 2 : 0,
+	retries: env.CI ? 2 : 0,
 	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 1 : undefined,
+	workers: 1,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: "list",
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -28,15 +29,15 @@ export default defineConfig({
 	projects: [
 		{
 			name: "chromium",
-			use: { ...devices["Desktop Chrome"],headless:false },
+			use: { ...devices["Desktop Chrome"] },
 		},
-		{
-			name: "firefox",
-			use: { ...devices["Desktop Firefox"] },
-		},
-		{
-			name: "webkit",
-			use: { ...devices["Desktop Safari"] },
-		},
+		// {
+		// 	name: "firefox",
+		// 	use: { ...devices["Desktop Firefox"] },
+		// },
+		// {
+		// 	name: "webkit",
+		// 	use: { ...devices["Desktop Safari"] },
+		// },
 	],
 });
--- test/web/unittest.ts
+++ test/web/unittest.ts
@@ -1,10 +1,15 @@
 import { readFileSync } from "fs";
 import swc from "@swc/core";
 import { expect, Request, Route, test as base } from "@playwright/test";
+import v8toIstanbul from "v8-to-istanbul";
+import libReport from "istanbul-lib-report";
+import reports from "istanbul-reports";
+import libCoverage from "istanbul-lib-coverage";
 
 export { expect };
 
 const swcrc = JSON.parse(readFileSync(".swcrc", "utf8"));
+swcrc.sourceMaps = true;
 
 const baseURL = "http://localhost/";
 
@@ -14,28 +19,71 @@ const BlankHTML = {
 	body: "<html><head></head><body></body></html>",
 };
 
+const transformed = new Map<string, any>();
+
 function loadIndexAndModule(route: Route, request: Request) {
 	const url = request.url();
 	if (url === baseURL) {
 		return route.fulfill(BlankHTML);
 	}
 	const path = decodeURIComponent(url.slice(baseURL.length));
-	const { code: body } = swc.transformFileSync(path, swcrc);
-	return route.fulfill({ body, contentType: "text/javascript" });
+	const source = readFileSync(path, "utf8");
+	swcrc.sourceFileName = path;
+	const { code, map } = swc.transformSync(source, swcrc);
+	transformed.set(url, { source, map });
+	return route.fulfill({ body: code, contentType: "text/javascript" });
+}
+
+let coverages: any[];
+
+const reportConfig = {
+	skipEmpty: false,
+	skipFull: false,
+	maxCols: process.stdout.columns || 100,
+};
+
+async function reportCoverage() {
+	const coverageMap = libCoverage.createCoverageMap();
+
+	for (const coverage of coverages) {
+		const { source, map } = transformed.get(coverage.url);
+		const sources = {
+			source: coverage.source,
+			sourceMap: { sourcemap: JSON.parse(map) },
+			originalSource: source,
+		};
+		const converter = v8toIstanbul(".", undefined, sources);
+		await converter.load();
+		converter.applyCoverage(coverage.functions);
+		coverageMap.merge(converter.toIstanbul());
+	}
+
+	const context = libReport.createContext({
+		coverageMap,
+		dir: "playwright-report",
+	});
+
+	reports.create("html", reportConfig).execute(context);
 }
 
 export const test = base.extend({
 	page: async ({ page }, use) => {
 		await page.route(`${baseURL}**/*`, loadIndexAndModule);
 		await page.goto(baseURL);
+		const browser = page.context().browser()!;
 
 		// Coverage APIs are only supported on Chromium-based browsers.
-		if (page.context().browser()!.browserType().name() !== "chromium") {
+		if (browser.browserType().name() !== "chromium") {
 			return use(page);
+		}
+
+		if (!coverages) {
+			coverages = [];
+			browser.on("disconnected", reportCoverage);
 		}
 
 		await page.coverage.startJSCoverage();
 		await use(page);
-		const coverage = await page.coverage.stopJSCoverage();
+		coverages.push(...await page.coverage.stopJSCoverage());
 	},
 });
