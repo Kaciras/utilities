c017c8044af81fa1fd3c8bbd3a2c44d09ae453c7
Kaciras <Kaciras@protonmail.com>
2024-06-26 16:02:16 +0800

    remove StackOverflow 7065321 implementation, as it fail with out-of-stack error

--- benchmark/cartesian.ts
+++ benchmark/cartesian.ts
@@ -13,11 +13,6 @@ const dataSets = {
 	small: [[11, 22], [33, 44]],
 };
 
-// @ts-ignore https://stackoverflow.com/a/43053803/7065321
-const f = (a: any[], b: any[]) => [].concat(...a.map(d => b.map(e => [].concat(d, e))));
-// @ts-ignore
-const SoF_7065321 = (a: any, b: any, ...c: any[]) => (b ? SoF_7065321(f(a, b), ...c) : a);
-
 function drain(generator: Iterable<unknown>) {
 	// noinspection StatementWithEmptyBodyJS
 	for (const _ of generator) /* No-op */;
@@ -31,8 +26,6 @@ export default defineSuite({
 		const src = dataSets[scene.params.input];
 		const oSrc = src.map((v, i) => [`Key_${i}`, v] as const);
 
-		// @ts-expect-error It works, just not TS compliant.
-		// scene.bench("SoF_7065321", () => SoF_7065321(...src));
 		scene.bench("array", () => drain(cartesianArray(src)));
 		scene.bench("object", () => drain(cartesianObject(oSrc)));
 	},
