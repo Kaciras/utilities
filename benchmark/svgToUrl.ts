7777f929df564927c5e46661bab9fbfcb9b6aa7f
Kaciras <Kaciras@protonmail.com>
2024-07-08 19:08:18 +0800

    add benchmark for `svgToUrl`

---
+++ benchmark/svgToUrl.ts
@@ -0,0 +1,26 @@
+import { readFileSync } from "fs";
+import { defineSuite, Profiler } from "esbench";
+import { svgToUrl } from "../src/codec.js";
+
+const svgBuf = readFileSync("test/fixtures/website.svg");
+const svgText = svgBuf.toString();
+
+const dataSizeProfiler: Profiler = {
+	onStart: ctx => ctx.defineMetric({
+		key: "size",
+		format: "{dataSize}",
+		analysis: 1,
+		lowerIsBetter: true,
+	}),
+	async onCase(_, case_, metrics) {
+		metrics.size = (await case_.invoke()).length;
+	},
+};
+
+export default defineSuite({
+	profilers: [dataSizeProfiler],
+	setup(scene) {
+		scene.bench("svgToUrl", () => svgToUrl(svgText));
+		scene.bench("base64", () => svgBuf.toString("base64"));
+	},
+});
